d1555675e96ee303bc770111a105ed0219e65466
CookieHCl <nhc7502@snu.ac.kr>
2025-06-13 13:59:13 +0900

    Use env to set key

--- app/useKakaoLoader.tsx
+++ app/useKakaoLoader.tsx
@@ -1,6 +1,11 @@
 import { useKakaoLoader as useKakaoLoaderOrigin } from "react-kakao-maps-sdk"
 
 export default function useKakaoLoader() {
+  const key = process.env.NEXT_PUBLIC_KAKAO_APP_KEY!;
+  if (!key) {
+    throw new Error("NEXT_PUBLIC_KAKAO_APP_KEY가 설정되지 않았습니다.");
+  }
+
   useKakaoLoaderOrigin({
     /** 
      * ※주의※ appkey의 경우 본인의 appkey를 사용하셔야 합니다.
@@ -8,7 +13,7 @@ export default function useKakaoLoader() {
      * 
      * @참고 https://apis.map.kakao.com/web/guide/
      */
-    appkey: "MY_APP_KEY",
+    appkey: key,
     libraries: ["clusterer", "drawing", "services"],
   })
 }
