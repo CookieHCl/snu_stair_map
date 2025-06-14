831cfbfeb85a41fd8806293e68818279b815ae58
CookieHCl <nhc7502@snu.ac.kr>
2025-06-14 14:12:54 +0900

    Deletable 아닐때 마커 클릭 못 하도록 변경

--- components/CoordinateMarkers.tsx
+++ components/CoordinateMarkers.tsx
@@ -1,4 +1,3 @@
-
 import { Circle } from "react-kakao-maps-sdk"
 import { IndexedCoordinate } from "@/types/coordinate"
 
@@ -20,11 +19,10 @@ export default function CoordinateMarkers({ coordinates, removeCoordinate, delet
         strokeOpacity={1.0}
         fillColor={coord.is_stair ? "#00FF33" : "#FF0000"}
         fillOpacity={0.15}
-        onClick={() => {
-          if (!deletable) return; // 삭제 가능하지 않으면 아무 동작도 하지 않음
+        onClick={deletable ? () => {
           kakao.maps.event.preventMap();
-          removeCoordinate?.(coord.index)
-        }} // 클릭 시 해당 좌표 제거
+          removeCoordinate?.(coord.index);
+        } : undefined} // 클릭 시 해당 좌표 제거
       />
     ))}
   </>
