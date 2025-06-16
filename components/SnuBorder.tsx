815906572363ec6f98242c08b3bd0ea153120d71
CookieHCl <nhc7502@snu.ac.kr>
2025-06-16 13:28:48 +0900

    Fix border not working

--- components/SnuBorder.tsx
+++ components/SnuBorder.tsx
@@ -8,15 +8,17 @@ interface SNUBorderProps {
 }
 
 export default function SNUBorder({ onMouseStateChange }: SNUBorderProps) {
-  return <Polygon
-    path={snu_border as IndexedCoordinate[]}
-    strokeColor="#00BBFF"
-    strokeOpacity={0}
-    strokeWeight={0.1}
-    fillColor="#00BBFF"
-    fillOpacity={0.000000001}
-    onMouseover={() => onMouseStateChange?.(true)}
-    onMouseout={() => onMouseStateChange?.(false)}
-    onClick={() => onMouseStateChange?.(true)}
-  />
+  return <div style={{ zIndex: 100 }}>
+    <Polygon
+      path={snu_border as IndexedCoordinate[]}
+      strokeColor="#00BBFF"
+      strokeOpacity={0}
+      strokeWeight={0.1}
+      fillColor="#00BBFF"
+      fillOpacity={0.000000001}
+      onMouseover={() => onMouseStateChange?.(true)}
+      onMouseout={() => onMouseStateChange?.(false)}
+      onClick={() => onMouseStateChange?.(true)}
+    />
+  </div>
 }
